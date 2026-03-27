bd56822f35aeedd532c65319c644c3601041ef62
DimkaZ <krepish1975@gmail.com>
2026-03-27 10:22:11 +0300

    техническое задание скрипт

--- BD2.sql
+++ BD2.sql
@@ -1,51 +1,90 @@
-CREATE TABLE movies (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(150) NOT NULL,
-    genre VARCHAR(50),
-    duration_min INT CHECK (duration_min > 0),
-    ticket_price NUMERIC(10, 2) DEFAULT 0.00,
-    rating VARCHAR(10),
-    is_3d BOOLEAN DEFAULT FALSE,
-    release_date DATE
-);
+import psycopg2
 
-INSERT INTO movies (title, genre, duration_min, ticket_price, rating, is_3d, release_date) VALUES
-('Интерстеллар', 'Фантастика', 169, 500.00, '12+', FALSE, '2014-11-06'),
-('Бегущий по лезвию 2049', 'Фантастика', 164, 600.00, '16+', TRUE, '2017-10-05'),
-('Мстители: Финал', 'Боевик', 181, 700.00, '12+', TRUE, '2019-04-24'),
-('Король Лев', 'Мультфильм', 118, 350.00, '0+', FALSE, '2019-07-19'),
-('Пираты Карибского моря', 'Боевик', 143, 550.00, '12+', FALSE, '2003-07-09'),
-('Тайна Коко', 'Мультфильм', 105, 400.00, '0+', FALSE, '2017-11-22');
+DB_CONFIG = {
+    'db_name': 'food_delivery_pro',
+    'user': 'postgres',
+    'password': 'Qweasdzx12qq',
+    'host': '127.0.0.1',
+    'port': '5432'
+}
 
-UPDATE movies
-SET ticket_price = ticket_price + 50
-WHERE genre = 'Фантастика';
+print('=== ДОБРО ПОЖАЛОВАТЬ В ТЕРМИНАЛ КАССИРА ===')
 
-DELETE FROM movies WHERE id = 3;
+try:
+    conn = psycopg2.connect(**DB_CONFIG)
+    cursor = conn.cursor()
 
-UPDATE movies
-SET rating = '16+'
-WHERE title = 'Интерстеллар';
+    # ПОИСК КЛИЕНТА
+    email = input('Введите email клиента: ').strip()
 
-SELECT title, ticket_price
-FROM movies
-WHERE duration_min > 120;
+    cursor.execute('SELECT id, full_name FROM customers WHERE email = %s', (email,))
+    user_record = cursor.fetchone()
 
-SELECT title, ticket_price
-FROM movies
-ORDER BY ticket_price ASC;
+    if user_record:
+        client_id = user_record[0]
+        client_name = user_record[1]
+        print(f'Рады снова видеть, {client_name}')
+    else:
+        print('Клиент не найден. Давайте зарегистрируем.')
+        client_name = input('Введите ФИО клиента: ').strip()
 
-SELECT COUNT(*) AS total_films
-FROM movies;
+        cursor.execute(
+            'INSERT INTO customers (full_name, email) VALUES (%s, %s) RETURNING id;',
+            (client_name, email)
+        )
+        client_id = cursor.fetchone()[0]
+        print(f'Клиент {client_name} успешно зарегистрирован (ID: {client_id}).')
 
-SELECT AVG(ticket_price) AS avg_price
-FROM movies;
 
-SELECT genre, COUNT(*) AS count_per_genre
-FROM movies
-GROUP BY genre;
+    # СОЗДАНИЕ КЛИЕНТА
+    cursor.execute(
+        'INSERT INTO orders (customer_id, status) VALUES (%s, "Принят") RETURNING id;',
+        (client_id,)
+    )
+    order_id = cursor.fetchone()[0]
+    print(f"\nОткрыт заказ №{order_id}, начинаем добавлять блюда.")
 
-SELECT genre, AVG(ticket_price) AS avg_price
-FROM movies
-GROUP BY genre
-HAVING AVG(ticket_price) > 350;
+    while True:
+        item_id_input = input("\nВведите ID блюда (или 0 чтобы пробить чек):")
+
+        if item_id_input == '0':
+            break
+        item_id = int(item_id_input)
+        quantity = int(("ВВедите кол-во порций:"))
+
+        cursor.execute(
+            "INSERT INTO order_items (order_id,item_id,quantity) VALUES (%s,%s,%s,);",
+            (order_id,item_id,quantity)
+        )
+
+        print (f"-> Добавлено: Блюдо ID{item_id}, {quantity} шт.")
+
+        conn. commit()
+        print("\n[успех] Заказ успешно сохранен в базе данных!")
+
+        print("\n" + "=" * 50)
+        print(f"Чек заказа №{order_id}")
+        print(f"Клиент: {client_name}" ({email}))
+        print("-" * 40)
+
+        receipt_query = 
+
+        SELECT m.item_name, oi.quantity, (oi.quantity * m.price) as total_price
+        FROM order_items order_id
+        JOIN menu_items m ON oi_item_id = m.id
+        WHERE oi.order_id = %s
+
+    except psycopg2.Error as db_error:
+    print(f"\n[Критическая ошибка БД] Транзакция прервана: {db_error}")
+    if conn:
+        conn.rollback()
+        print("Изменения отменены")
+    except ValueError:
+print("\n[ошибка ввода] Вы ввели буквы вместо цифр! Программа Завершена.")
+if conn:
+    conn.rollback()
+finally:
+    if 'conn' in locals() and conn:
+    cursor.close()
+conn.close()
+print("сеанс работы с терминалом завершен.")
